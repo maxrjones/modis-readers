da4117be4bf8fb819a5f0851466016cc255431ec
Max Jones <14077947+maxrjones@users.noreply.github.com>
2024-08-21 16:00:50 -0400

    Add MVCE

---
+++ mvce.py
@@ -0,0 +1,17 @@
+import os
+
+import earthaccess
+from netCDF4 import Dataset
+
+earthaccess.login()
+
+# Download some MODIS/Terra Thermal Anomalies/Fire 5-Min L2 Swath 1km data (100 files)
+results = earthaccess.search_data(
+    concept_id="C2271754179-LPCLOUD", count=1, temporal=("2015-06-01", "2015-12-31")
+)
+file = earthaccess.download(results, "earthaccess_data")[0]
+
+cwd = os.getcwd()
+fh = Dataset(os.path.join(cwd, file), mode="r")
+fire_mask = fh.variables["fire mask"][:].data
+FP_line = fh.variables["FP_line"][:].data
